41d77f7cb2428766a5a5fc0ee754e5181eab2f35
lsm <2217046488@qq.com>
2019-07-12 14:39:04 +0800

    首页

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import Vue from 'vue'
 // 加载路由模块
 import VueRouter from 'vue-router'
 import Login from '@/views/login'
+import Home from '@/views/home'
 // 注册到Vue中才可以使用
 Vue.use(VueRouter)
 const router = new VueRouter({
   // 配置路由规则
   routes: [
-    { name: 'login', path: '/login', component: Login }
+    { name: 'login', path: '/login', component: Login },
+    { name: 'home', path: '/', component: Home }
   ]
 })
 // 导出
